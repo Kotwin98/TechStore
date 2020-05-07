6d48d501aee88ab54f2f3f52a52183c24a36a373
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-07 13:19:18 +0200

    Import pages

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import HomePage from '../pages/HomePage';
+import FaqPage from '../pages/FaqPage';
+import PolicyPage from '../pages/PolicyPage';
+import ContactPage from '../pages/ContactPage';
+import NotFoundPage from '../pages/NotFoundPage';
+
 const Main = () => (
     <Switch>
         <Route path="/" exact component={HomePage} />
