876d1d8a7ce4788d1da50c480f9d6dacbe71bde6
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-07 12:56:30 +0200

    Import layout files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 
+import Header from './components/layout/Header';
+import Main from './components/layout/Main';
+import Footer from './components/layout/Footer';
 import './App.css';
 
 function App() {
