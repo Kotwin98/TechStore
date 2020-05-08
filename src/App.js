4c5d613ca76aea2204b580d07720ea388b642952
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-08 09:21:55 +0200

    Change css to scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from './components/layout/Header/Header';
 import Main from './components/layout/Main/Main';
 import Footer from './components/layout/Footer/Footer';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
