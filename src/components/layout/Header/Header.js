cc56011ad1f4d6fba6d9c4ab5a9d114440019370
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-08 09:54:45 +0200

    Import Header.scss

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './Header.scss';
+
 const Header = () => (
-    <div>
-        <NavLink exact to="/">
+    <div className="header-container">
+        <NavLink exact to="/" className="header-link">
             HOME
         </NavLink>
-        <NavLink exact to="/faq">
+        <NavLink exact to="/faq" className="header-link">
             FAQ
         </NavLink>
-        <NavLink exact to="/policy">
+        <NavLink exact to="/policy" className="header-link">
             STORE POLICY
         </NavLink>
-        <NavLink exact to="/contact">
+        <NavLink exact to="/contact" className="header-link">
             CONTACT
         </NavLink>
     </div>
