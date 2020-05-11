65d4e0776cc4fbfb43eb2bf983038c7c91b43e1d
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-11 17:28:24 +0200

    Add policy page content

--- src/components/pages/Policy/PolicyPage.js
+++ src/components/pages/Policy/PolicyPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import './PolicyPage.scss';
+
 const PolicyPage = () => (
     <div>
-        <p>Policy</p>
+        <h1>Store Policy</h1>
+        <p className="policy-content">$1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis ante id ultrices aliquet. Proin augue diam, aliquet a lacinia ut, consequat id lacus. Pellentesque lacus odio, facilisis et lectus non, bibendum vulputate orci. Cras pretium elit vel arcu consequat volutpat. Nam auctor, urna quis dignissim sagittis, velit tortor ultricies felis, in mollis libero neque quis justo.</p>
+        <p className="policy-content">$1.1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis ante id ultrices aliquet. Proin augue diam, aliquet a lacinia ut, consequat id lacus. Pellentesque lacus odio, facilisis et lectus non, bibendum vulputate orci. Cras pretium elit vel arcu consequat volutpat. Nam auctor, urna quis dignissim sagittis, velit tortor ultricies felis, in mollis libero neque quis justo.</p>
+        <p className="policy-content">$1.2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis ante id ultrices aliquet. Proin augue diam, aliquet a lacinia ut, consequat id lacus.Pellentesque lacus odio, facilisis et lectus non, bibendum vulputate orci. Cras pretium elit vel arcu consequat volutpat. Nam auctor, urna quis dignissim sagittis, velit tortor ultricies felis, in mollis libero neque quis justo.</p>
+        <p className="policy-content">$1.3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis ante id ultrices aliquet. Proin augue diam, aliquet a lacinia ut, consequat id lacus. Pellentesque lacus odio, facilisis et lectus non, bibendum vulputate orci. Cras pretium elit vel arcu consequat volutpat. Nam auctor, urna quis dignissim sagittis, velit tortor ultricies felis, in mollis libero neque quis justo.</p>
     </div>
 );
 
