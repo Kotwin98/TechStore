a348fccf91a55dd5acb685b78a21108a7387ca62
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-11 10:17:23 +0200

    Add Faq page content

--- src/components/pages/Faq/FaqPage.js
+++ src/components/pages/Faq/FaqPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import './FaqPage.scss';
+
 const FaqPage = () => (
     <div>
-        <p>Faq</p>
+        <h1>FAQ</h1>
+        <p className="faq-content">Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis consequat turpis.</p>
+        <p className="faq-content">A: Vestibulum ac molestie dui. Curabitur odio velit, mattis quis massa et, pellentesque bibendum ligula. Mauris dictum enim at nisi varius aliquet.</p>
+        <p className="faq-content">Q: Nulla faucibus dui arcu, nec suscipit mauris faucibus eu.</p>
+        <p className="faq-content">A: Duis in tortor nec orci mattis eleifend eu a nunc. Aliquam erat volutpat. Phasellus finibus sollicitudin bibendum.</p>
+        <p className="faq-content">Q: Sed vel elit in ex sagittis facilisis tempus non lectus. Sed ut leo orci.</p>
+        <p className="faq-content">A: Maecenas euismod sollicitudin ante, nec lobortis est rhoncus quis. In mollis velit quis ligula efficitur efficitur. Donec dignissim ante et erat malesuada, vel sollicitudin elit feugiat.</p>
+        <p className="faq-content">Q: Suspendisse hendrerit congue nisi, vitae eleifend odio interdum et.</p>
+        <p className="faq-content">A: Quisque sollicitudin arcu rhoncus sem viverra, non rutrum felis rhoncus. Vivamus dui nulla, facilisis sed velit a, pharetra fermentum elit.</p>
     </div>
 );
 
