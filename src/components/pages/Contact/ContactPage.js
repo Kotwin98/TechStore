9f930974e21a83def9dfa30f2f46bb35bed7cdb3
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-05-20 12:20:57 +0200

    Add ContactPage content

--- src/components/pages/Contact/ContactPage.js
+++ src/components/pages/Contact/ContactPage.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookSquare, faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import './ContactPage.scss';
+
 const ContactPage = () => (
     <div>
-        <p>Contact</p>
+        <h1>Contact</h1>
+        <div className="contact-wrapper">
+            <p><strong>TechStore</strong></p>
+            <p><i>4323 Long Street</i></p>
+            <p>E-mail: <FontAwesomeIcon icon={faEnvelope} /> office@techstore.com</p>
+            <p>Mobile Number: <FontAwesomeIcon icon={faMobileAlt} /> 283-482-847</p>
+            <a href="#" target="_blank" className="contact-icon"><FontAwesomeIcon icon={faFacebookSquare} /></a>
+            <a href="#" target="_blank" className="contact-icon"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="#" target="_blank" className="contact-icon"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="#" target="_blank" className="contact-icon"><FontAwesomeIcon icon={faLinkedin} /></a>
+        </div>
     </div>
 );
 
